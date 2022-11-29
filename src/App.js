896ad8c9e538a6ed6715ba14ea3f1885d1bc2458
kenan aljaber <kenan.aljaber.p@gmail.com>
2022-11-28 19:00:08 -0500

    adding the responsivness reature to the site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './styles/index.css';
-import About from "./Pages/Contact";
 import MyNavBar from "./Components/MyNavBar";
 import Footer from "./Components/Footer";
 import Welcome from "./Pages/Welcome";
@@ -16,21 +14,21 @@ import Contact from "./Pages/Contact";
 function App() {
   return (
     <div className="App">
-      
-     
-      
+
+
+
       <Router>
-      <MyNavBar/>
-      <Routes>  
-        <Route path="/" element={<Welcome/> }/>
-        <Route path="/imc-calculator" element={<ImcCalculatorPage/> }/>
-        <Route path="/age-calculator" element={<AgeCalculator/> }/>
-        <Route path="/clock" element={<Clock/> }/>
-        <Route path="/about-me" element={<AboutMe/> }/>
-        <Route path="/contact" element={<Contact/> }/>
+        <MyNavBar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/imc-calculator" element={<ImcCalculatorPage />} />
+          <Route path="/age-calculator" element={<AgeCalculator />} />
+          <Route path="/clock" element={<Clock />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-      <Footer/>
+      <Footer />
 
     </div>
   );
